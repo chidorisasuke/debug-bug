91d7724df6242f808bc0f6d0ace0f09d1e0fede8
chidorisasuke <yahyabachtiar03@gmail.com>
2026-09-13 23:26:29 +0700

    Initial C++ project setup

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int findLargest(const int numbers[], int size)
+{
+    int largest = numbers[0];
+
+    for (int i = 1; i < size; ++i)
+    {
+        if (numbers[i] > largest)
+        {
+            largest = numbers[i];
+        }
+    }
+
+    return largest;
+}
+
+int main()
+{
+    int numbers[] = {-25, -10, -3, -17, -8};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    int largest = findLargest(numbers, size);
+
+    std::cout << "Nilai terbesar: " << largest << '\n';
+
+    return 0;
+}
